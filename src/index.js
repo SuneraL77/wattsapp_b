7da55a94c03ba0085b69bf7f26baab701217aacb
sunera Harshana <107272662+SuneraL77@users.noreply.github.com>
2023-11-28 22:52:55 +0530

    logger add

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 
 import app from "./app.js";
-import dotenv from "dotenv"
-
-//dotEnv config
-dotenv.config();
-
+import logger from "./configs/logger.js";
 //create express app
 
 
@@ -12,5 +8,5 @@ dotenv.config();
 const PORT = process.env.PORT ||8000;
 
 app.listen(PORT, () =>{
-    console.log(`server is listening at ${PORT}`)
+    logger.info(`server is listening at ${PORT}`)
 })
